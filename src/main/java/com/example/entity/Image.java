64289b259d3523d7964d2d44628fa1542e9b8b49
DeadEndjke <perceptionarmy@icloud.com>
2023-02-23 01:04:34 +0700

    add Image annotations

--- src/main/java/com/example/entity/Image.java
+++ src/main/java/com/example/entity/Image.java
@@ -1,10 +1,24 @@
 package com.example.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Lob
+    @Column(columnDefinition = "LONGLOB")
     private byte[] imageBytes;
+    @JsonIgnore
     private Long userId;
+    @JsonIgnore
     private Long postId;
 
 }
